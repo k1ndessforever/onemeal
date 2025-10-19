6f8523fca0b3f9cfe4081bc8d033ed15b968961b
Yash Pandit <yash.pandit.dev01@gmail.com>
2025-10-19 09:28:22 +0530

    Update eslint.config.mjs

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,10 +1,6 @@
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { FlatCompat } from "@eslint/eslintrc";
-import nextPlugin from "@next/eslint-plugin-next";
-import eslint from "@typescript-eslint/eslint-plugin";
-import tseslint from "typescript-eslint";
-import tsParser from "@typescript-eslint/parser";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -15,47 +11,6 @@ const compat = new FlatCompat({
 
 const eslintConfig = [
   ...compat.extends("next/core-web-vitals", "next/typescript"),
-  {
-    ignores: [
-      "node_modules/**",
-      ".next/**",
-      "out/**",
-      "build/**",
-      "next-env.d.ts",
-    ],
-  },
 ];
 
-export default [
-  {
-    files: ["**/*.ts", "**/*.tsx"],
-    plugins: {
-      "@next": nextPlugin,
-      "@typescript-eslint": tseslint,
-    },
-    languageOptions: {
-      parser: tsParser,
-      parserOptions: {
-        project: true,
-        tsconfigRootDir: import.meta.dirname,
-      },
-    },
-    rules: {
-      "@typescript-eslint/no-explicit-any": "error",
-      "@next/next/no-html-link-for-pages": "error",
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        {
-          argsIgnorePattern: "^_",
-          varsIgnorePattern: "^_",
-        },
-      ],
-    },
-  },
-  {
-    plugins: {
-      "@next": nextPlugin,
-      "@typescript-eslint": eslint
-    }
-  }
-];
+export default eslintConfig;
