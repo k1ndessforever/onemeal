33d3499e7731ce0355a240c720bafd898c7d873d
Yash Pandit <yash.pandit.dev01@gmail.com>
2025-10-19 09:40:56 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   
-  // Disable ESLint during builds (you can still run it manually)
+  // Add this to disable ESLint warnings
   eslint: {
     ignoreDuringBuilds: true,
   },
